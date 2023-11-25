372fa420f7c1cccef19c666f83bf472882112fff
Lee Jae Sun <ze_s96@naver.com>
2023-11-25 22:00:44 +0900

    메인페이지 완료, css 수정 - 날짜 이동 버튼 & 새 일기쓰기 버튼 & 수정하기 버튼 구현 - 메인페이지 리스트 출력(더미데이터 이용) - 리스트 출력 옵션(날짜순, 감정순)

---
+++ src/components/DiaryItem.js
@@ -0,0 +1,42 @@
+import { useNavigate } from "react-router-dom";
+import MyButton from "./MyButton";
+
+const DirayItem = ({ id, emotion, content, date }) => {
+    const navigate = useNavigate();
+    const strDate = new Date(parseInt(date)).toLocaleDateString();
+    const getDetail = () => {
+        navigate(`/diary/${id}`);
+    }
+    const goedit = () => {
+        navigate(`/edit/${id}`);
+    }
+    return (
+        <div className="DiaryItem">
+            <div
+                className={[
+                    "emotion_img_wrapper",
+                    `emotion_img_wrapper_${emotion}`,
+                ].join(" ")} onClick={getDetail}
+            >
+                <img
+                    src={
+                        process.env.PUBLIC_URL + `assets/emotion${emotion}.png`
+                    }
+                />
+            </div>
+
+            <div className="info_wrapper" onClick={getDetail}>
+                <div className="diary_date">{strDate}</div>
+                <div className="diary_content_preview">
+                    {content.slice(0, 25)}
+                </div>
+            </div>
+
+            <div className="btn_wrapper">
+                <MyButton text={"수정하기"} onClick={goedit}/>
+            </div>
+        </div>
+    );
+};
+
+export default DirayItem;
