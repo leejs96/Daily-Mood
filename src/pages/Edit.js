eb1b04ca0dd838cede35139bab6d9e6993d9a99a
Lee Jae Sun <ze_s96@naver.com>
2023-12-01 19:26:11 +0900

    배포준비 - 타이틀명 변경
    - 각 페이지 타이틀명 변경
    - 페이지 설명 변경(desciption)

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -11,6 +11,11 @@ const Edit = () => {
     const diaryList = useContext(DiaryStateContext);
 
     useEffect(() => {
+        const titleElement = document.getElementsByTagName('title')[0];
+        titleElement.innerHTML = `감정 일기장 - 일기 수정하기`;
+    }, []);
+
+    useEffect(() => {
         if (diaryList.length >= 1) {
             const targetDiary = diaryList.find(
                 (it) => parseInt(it.id) === parseInt(id)
